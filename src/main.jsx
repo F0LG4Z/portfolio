b65f676c07c7a12730c98f16400904eca839835a
F0LG4Z <ritaffolgado@gmail.com>
2024-05-01 00:11:32 +0100

    router basename

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import ProjetoVideo from './features/projetos/components/ProjetoVideo.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/portfolio/">
       <Routes>
         <Route path='/' element={<App />}> 
           <Route index element={<Inicio />} />
